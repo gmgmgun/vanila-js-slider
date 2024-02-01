d5c88550cd0c8ee1fc001e3bb83b5631132668f7
dongminlee <gmgmgun@gmail.com>
2024-02-01 18:05:40 +0900

    Add: 인디케이터 기능 추가
    - 인디케이터를 통해 사용자가 몇번째 슬라이드를 보고 있는지 알려줌.
    - 인디케이터 클릭 이벤트를 통해 해당 슬라이드로 이동.

--- app.js
+++ app.js
@@ -1,39 +1,98 @@
 class App {
   constructor() {
     this.slider = document.querySelectorAll('#slider img');
+    this.indicators = document.querySelectorAll('.indicator');
     this.isAuto = true;
     this.totalSlides = this.slider.length;
-    this.curIdx = 0;
+    this.currentSlide = 0;
     this.autoSlideInterval = null;
-    this.showSlide(this.curIdx);
+
     this.startAutoSlide(this.isAuto);
     this.updateAutoPlayButton();
+    this.updateSlider();
 
     document
       .getElementById('auto-play')
       .addEventListener('click', () => this.toggleAutoPlay());
+
     document
       .getElementById('next')
-      .addEventListener('click', () => this.changeSlide(1));
+      .addEventListener('click', () =>
+        this.changeSlide(this.currentSlide, true)
+      );
+
     document
       .getElementById('prev')
-      .addEventListener('click', () => this.changeSlide(-1));
+      .addEventListener('click', () =>
+        this.changeSlide(this.currentSlide, false)
+      );
+
+    this.indicators.forEach((indicator, idx) => {
+      indicator.addEventListener('click', () => this.showSlide(idx));
+    });
+  }
+
+  changeSlide(index, isNext) {
+    this.resetAutoSlide();
+
+    if (isNext) {
+      if (index === this.totalSlides - 1) {
+        this.currentSlide = 0;
+      } else {
+        this.currentSlide = index + 1;
+      }
+    } else {
+      if (index === 0) {
+        this.currentSlide = this.totalSlides - 1;
+      } else {
+        this.currentSlide = index - 1;
+      }
+    }
+
+    this.updateSlider();
   }
 
   showSlide(index) {
-    this.slider.forEach((img, i) => {
-      img.style.transform = `translateX(${100 * (i - index)}%)`;
+    this.resetAutoSlide();
+
+    this.currentSlide = index;
+
+    this.updateSlider();
+  }
+
+  updateSlider() {
+    this.slider.forEach((slide, index) => {
+      slide.style.transform = `translateX(${
+        100 * (index - this.currentSlide)
+      }%)`;
+    });
+
+    this.indicators.forEach((indicator, index) => {
+      if (index === this.currentSlide) {
+        indicator.classList.add('black');
+        indicator.classList.remove('white');
+      } else {
+        indicator.classList.add('white');
+        indicator.classList.remove('black');
+      }
     });
   }
 
   startAutoSlide() {
     this.autoSlideInterval = setInterval(() => {
-      this.changeSlide(1);
+      this.changeSlide(this.currentSlide, true);
     }, 2000);
   }
 
   stopAutoSlide() {
     clearInterval(this.autoSlideInterval);
+  }
+
+  resetAutoSlide() {
+    if (this.isAuto) {
+      this.stopAutoSlide();
+      this.startAutoSlide();
+    }
   }
 
   toggleAutoPlay() {
@@ -45,12 +104,6 @@ class App {
     } else {
       this.stopAutoSlide();
     }
-  }
-
-  changeSlide(direction) {
-    this.curIdx =
-      (this.curIdx + direction + this.totalSlides) % this.totalSlides;
-    this.showSlide(this.curIdx);
   }
 
   updateAutoPlayButton() {
