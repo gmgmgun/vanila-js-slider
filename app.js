369db435d5fc784c35b31cffd0f2a0152713116c
dongminlee <gmgmgun@gmail.com>
2024-02-01 15:38:31 +0900

    Add: 자동 재생 기능 추가
    - 어플리케이션 실행 시 2초 간격으로 이미지 슬라이드.
    - 멈춤 버튼 클릭 시 자동 재생을 멈추고 재생 아이콘으로 변경.
    - 재생 버튼 클릭 시 자동 재생을 시작하고 멈춤 아이콘으로 변경.

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 class App {
   constructor() {
     this.slider = document.querySelectorAll('#slider img');
+    this.isAuto = true;
     this.totalSlides = this.slider.length;
     this.curIdx = 0;
+    this.autoSlideInterval = null;
     this.showSlide(this.curIdx);
+    this.startAutoSlide(this.isAuto);
+    this.updateAutoPlayButton();
 
+    document
+      .getElementById('auto-play')
+      .addEventListener('click', () => this.toggleAutoPlay());
     document
       .getElementById('next')
       .addEventListener('click', () => this.changeSlide(1));
@@ -19,10 +26,39 @@ class App {
     });
   }
 
+  startAutoSlide() {
+    this.autoSlideInterval = setInterval(() => {
+      this.changeSlide(1);
+    }, 2000);
+  }
+
+  stopAutoSlide() {
+    clearInterval(this.autoSlideInterval);
+  }
+
+  toggleAutoPlay() {
+    this.isAuto = !this.isAuto;
+    this.updateAutoPlayButton();
+
+    if (this.isAuto) {
+      this.startAutoSlide();
+    } else {
+      this.stopAutoSlide();
+    }
+  }
+
   changeSlide(direction) {
     this.curIdx =
       (this.curIdx + direction + this.totalSlides) % this.totalSlides;
     this.showSlide(this.curIdx);
+  }
+
+  updateAutoPlayButton() {
+    const btnAutoPlay = document.getElementById('auto-play-img');
+
+    btnAutoPlay.src = this.isAuto
+      ? '/data/images/auto-pause.png'
+      : '/data/images/auto-start.png';
   }
 }
 
